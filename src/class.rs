fcbe3a491a71ff9f9f53b9b962441752c8e5c51a
juliarose <julialunarose@gmail.com>
2022-04-26 16:13:04 -0400

    deserialize string enums

--- src/class.rs
+++ src/class.rs
@@ -1,7 +1,7 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use strum_macros::{Display, EnumString, EnumIter};
 
-#[derive(Debug, Serialize, Hash, Eq, PartialEq, Display, EnumString, EnumIter, Clone, Copy)]
+#[derive(Debug, Deserialize, Serialize, Hash, Eq, PartialEq, Display, EnumString, EnumIter, Clone, Copy)]
 pub enum Class {
     Scout,
     Soldier,
