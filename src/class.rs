c63cb67e2207f2aa4d60f26299e367703fad3d25
juliarose <julialunarose@gmail.com>
2022-04-26 20:32:07 -0400

    serialize lowercase

--- src/class.rs
+++ src/class.rs
@@ -3,13 +3,22 @@ use strum_macros::{Display, EnumString, EnumIter};
 
 #[derive(Debug, Deserialize, Serialize, Hash, Eq, PartialEq, Display, EnumString, EnumIter, Clone, Copy)]
 pub enum Class {
+    #[serde(alias = "scout")]
     Scout,
+    #[serde(alias = "soldier")]
     Soldier,
+    #[serde(alias = "pyro")]
     Pyro,
+    #[serde(alias = "demoman")]
     Demoman,
+    #[serde(alias = "heavy")]
     Heavy,
+    #[serde(alias = "engineer")]
     Engineer,
+    #[serde(alias = "medic")]
     Medic,
+    #[serde(alias = "sniper")]
     Sniper,
+    #[serde(alias = "spy")]
     Spy,
 }
--- src/craft_class.rs
+++ src/craft_class.rs
@@ -4,17 +4,24 @@ use strum_macros::{Display, EnumString, EnumIter};
 #[derive(Debug, Deserialize, Serialize, Hash, Eq, PartialEq, Display, EnumString, EnumIter, Clone, Copy)]
 pub enum CraftClass {
     #[strum(serialize = "weapon")]
+    #[serde(rename = "weapon")]
     Weapon,
     #[strum(serialize = "hat")]
+    #[serde(rename = "hat")]
     Hat,
     #[strum(serialize = "craft_bar")]
+    #[serde(rename = "craft_bar")]
     CraftBar,
     #[strum(serialize = "haunted_hat")]
+    #[serde(rename = "haunted_hat")]
     HauntedHat,
     #[strum(serialize = "tool")]
+    #[serde(rename = "tool")]
     Tool,
     #[strum(serialize = "craft_token")]
+    #[serde(rename = "craft_token")]
     CraftToken,
     #[strum(serialize = "supply_crate")]
+    #[serde(rename = "supply_crate")]
     SupplyCrate,
 }
